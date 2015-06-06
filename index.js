612dddf03918e65f2c010c16601af196740b64fb
kenshinhu <hujianxincom@gmail.com>
2015-06-06 15:56:39 +0800

    add .gitignore

--- index.js
+++ index.js
@@ -5,10 +5,28 @@ var request = require('./request');
 
 
 var args = process.argv.slice(2);
+var clearArgs = process.argv.slice(3);
 
 var pages = args[0] || 1;
+var clear = clearArgs[0];
 
-request.toPage(pages,function(){
-    process.exit(0);
-})
+var start = function(){
+    request.toPage(pages,function(){
+        process.exit(0);
+    })
+}
+
+if(clear==="--clear"){
+    console.log("Ready to ClearData");
+    request.clearData(function(){
+       start();
+    });
+}else{
+    start();
+}
+
+
+
+
+
 
